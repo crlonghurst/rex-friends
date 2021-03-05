f288eb5b4bf1efb58ff65275fa393106c4ccebde
rinelson456 <nel15031@byui.edu>
2021-03-04 17:12:20 -0700

    routes

--- app.js
+++ app.js
@@ -6,15 +6,16 @@ const mysql = require('mysql')
 const PORT = process.env.PORT || 5000 // So we can run on heroku || (OR) localhost:5000
 
 const app = express();
+const authRoutes = require('./routes/auth')
 
 
 
 app.use(express.static(path.join(__dirname, 'public')))
-.get('/', (req, res, next) => {
-res.render('pages/index', {title: 'Welcome to my CSE341 repo', path: '/'});
+    .get('/', (req, res, next) => {
+        res.render('pages/index', { title: 'Welcome to my CSE341 repo', path: '/' });
     })
-   .use((req, res, next) => {
-     // 404 page
-     res.render('pages/404', {title: '404 - Page Not Found', path: req.url})
-   })
-   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+    .use((req, res, next) => {
+        // 404 page
+        res.render('pages/404', { title: '404 - Page Not Found', path: req.url })
+    })
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
